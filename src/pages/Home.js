b968c66d6638410bbbddb47e51445769ebaaaedb
Kelly Coldren <shrtpvteacher@protonmail.com>
2025-04-03 17:44:38 -0700

    commented out video link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,10 +31,10 @@ const Home = () => {
         <Container className="py-5 d-flex flex-column align-items-center justify-content-center text-center fade-in" style={{ minHeight: "100vh" }}>
        <h2>Welcome to Seeds2Trees</h2>
         <h2>Please watch this short video on how to install MetaMask</h2>
-        <video style={{ width: "100%", maxWidth: "720px", margin: "1rem 0" }} controls>
-         <source src="/videos/install-metamask.mp4" type="video/mp4" />
+       {/* <video style={{ width: "100%", maxWidth: "720px", margin: "1rem 0" }} controls>
+         <source src="/videos/install-metamask.mp4" type="video/mp4" /> */ }
           Your browser does not support the video tag.
-        </video>
+       {/* </video> */}
         <p>
           After installing MetaMask, refresh this page to continue.
         </p>
