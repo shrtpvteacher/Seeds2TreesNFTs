50c6ae6c82bfed2c50d2eadd4f120df5231dc5ab
Kelly Coldren <shrtpvteacher@protonmail.com>
2025-04-15 21:24:53 -0700

    fundFaucet script was added

---
+++ scripts/fundFaucet.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const { ethers } = require("hardhat");
+
+async function main() {
+  const faucetAddress = process.env.FAUCET_CONTRACT_ADDRESS;
+  const amountInEth = "0.003"; // Amount to send
+
+  const [sender] = await ethers.getSigners();
+
+  console.log(`Sending ${amountInEth} ETH from ${sender.address} to ${faucetAddress}...`);
+
+  const tx = await sender.sendTransaction({
+    to: faucetAddress,
+    value: ethers.utils.parseEther(amountInEth),
+  });
+
+  await tx.wait();
+  console.log(`✅ Success! TX hash: ${tx.hash}`);
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
