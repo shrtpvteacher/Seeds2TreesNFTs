5290a1dca41a908a8d2cfb2778a51b0449b39afc
Kelly Coldren <shrtpvteacher@protonmail.com>
2025-04-22 23:45:42 -0700

    changed error handling in faucetstats

--- src/components/FaucetStatsCard.js
+++ src/components/FaucetStatsCard.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 import FaucetAbi from "../abis/Seeds2TreesFaucet.json";
+import { Spinner } from 'react-bootstrap'; // Import Spinner for loading
 
 const FaucetStatsCard = () => {
   const [faucetBalance, setFaucetBalance] = useState(null);
   const [loading, setLoading] = useState(true); // Track loading state
+  const [error, setError] = useState(null); // Track errors
 
   // Define the contract address directly in the component
   const contractAddress = process.env.REACT_APP_FAUCET_CONTRACT_ADDRESS;
@@ -15,9 +17,7 @@ const FaucetStatsCard = () => {
       setLoading(true); // Set loading to true before fetching
       try {
         if (!contractAddress) {
-          console.error("Contract address is missing");
-          setFaucetBalance(null);
-          return;
+          throw new Error("Contract address is missing");
         }
 
         const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -28,6 +28,7 @@ const FaucetStatsCard = () => {
         setFaucetBalance(ethers.utils.formatEther(balance)); // Convert the balance from wei to ETH
       } catch (err) {
         console.error("Failed to fetch faucet balance:", err);
+        setError("Failed to fetch faucet balance. Please try again later.");
         setFaucetBalance(null);
       } finally {
         setLoading(false); // Set loading to false after fetching
@@ -39,10 +40,16 @@ const FaucetStatsCard = () => {
 
   return (
     <div className="bg-warning shadow p-3 mb-5 rounded-3 p-6 w-full max-w-md mx-auto">
-      <h2 className="text-1xl font-semibold mb-2">Faucet Balance</h2>
-      <p>
-        {loading ? "Loading..." : <strong>{faucetBalance ? faucetBalance : "Error fetching balance"}</strong>} ETH
-      </p>
+      <h2 className="text-2xl font-semibold mb-4">Balance</h2>
+      {loading ? (
+        <Spinner animation="border" size="sm" /> // Show spinner while loading
+      ) : error ? (
+        <p className="text-danger">{error}</p> // Show error message if there's an issue
+      ) : (
+        <p>
+          <strong>{faucetBalance}</strong> ETH
+        </p>
+      )}
     </div>
   );
 };
